d9c5b9a8376cdaabda6695c44648afb990f4d42d
Tinnakorn Choompee <tinnakornchoompee@gmail.com>
2019-02-06 19:38:21 +0700

    Migrate Message

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -20,6 +20,21 @@ $factory->define(App\User::class, function (Faker $faker) {
         'email' => $faker->unique()->safeEmail,
         'email_verified_at' => now(),
         'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
+        'phone' => $faker->phoneNumber,
+        'image' => 'http://via.placeholder.com/150x150',
         'remember_token' => Str::random(10),
+    ];
+});
+
+$factory->define(App\Message::class, function (Faker $faker) {
+    do {
+        $from = rand(1,15);
+        $to   = rand(1,15);
+    } while ($from == $to);
+
+    return [
+        'from' => $from,
+        'to'   => $to,
+        'text' => $faker->sentence,
     ];
 });
